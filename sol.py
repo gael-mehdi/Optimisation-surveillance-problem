43f43fe2da6f21b90a7aca07ed57d6925b402438
gael-mehdi <gm.lelay@outlook.com>
2023-06-12 09:41:45 +0200

    solution 1

--- sol.py
+++ sol.py
@@ -37,6 +37,7 @@ def place_guards(grid):
                 guards.append((x, y))
     return guards
 
+"""
 # Exemple d'utilisation
 file_path = 'Instances-20230612/gr1.txt'
 grid = load_grid(file_path)
@@ -44,3 +45,31 @@ guards = place_guards(grid)
 print("Coordonnées des gardiens :")
 for guard in guards:
     print(guard)
+
+# Enregistrement des positions des gardiens dans le fichier sol1.txt
+with open('sol1.txt', 'w') as file:
+    for guard in guards:
+        file.write(f"{guard[0]} {guard[1]}\n")
+
+print("Positions des gardiens enregistrées dans sol1.txt.")
+"""
+
+# Boucle pour traiter toutes les instances
+for i in range(1, 17):
+    instance_path = f'Instances-20230612/gr{i}.txt'
+    solution_path = f'sol{i}.txt'
+
+    # Chargement de la grille
+    grid = load_grid(instance_path)
+
+    # Placement des gardiens
+    guards = place_guards(grid)
+
+    # Enregistrement des positions des gardiens dans le fichier de solution
+    with open(solution_path, 'w') as file:
+        file.write("EQUIPE lionel pepsi\n")
+        file.write(f"INSTANCE {i}\n")
+        for guard in guards:
+            file.write(f"{guard[0]} {guard[1]}\n")
+
+    print(f"Solution pour l'instance {instance_path} enregistrée dans {solution_path}.")
